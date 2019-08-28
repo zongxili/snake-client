7cbde6133828d124408a1cccc7997bfc2c0fc767
Zongxi Li <zongxiliwork@gmail.com>
2019-08-28 21:47:57 +0000

    Send Name function added

--- client.js
+++ client.js
@@ -1,11 +1,34 @@
+#!/usr/bin/env node
+process.stdout.write('\x07');
+
 const net = require('net');
 
-const conn = net.createConnection({
-  host: 'localhost', // change to IP address
-  port: 50541
-
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    // conn.write("Name: ZXL", ()=>{
+    //   console.log("Sent Name");
+    // });
+    conn.write("Name: ZXL");
+  });
 
   
-});
+  //Update the connect function to also attach an event handler to handle incoming data and console log it for the player.
+  conn.on('data', (data) => {
+    console.log('Message from server: ', data);
+  });
 
-conn.setEncoding('utf8'); // interpret data as text
+  return conn;
+}
+
+module.exports = { connect };
---
+++ play.js
@@ -0,0 +1,5 @@
+// play.js for the client side
+const { connect } = require('./client');
+
+console.log('Connecting ...');
+connect();
