090b436719fb5fbfc7b04ad8b76a29dd52194c78
Zongxi Li <zongxiliwork@gmail.com>
2019-08-28 22:31:25 +0000

    stdin added

--- client.js
+++ client.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 process.stdout.write('\x07');
-
 const net = require('net');
-
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
   
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
@@ -21,28 +19,22 @@ const connect = function() {
     // });
     conn.write("Name: ZXL");
 
-    // setTimeout(function(){ 
+    // setTimeout(function(){
     //   conn.write("Move: up");
     // }, 1000);
 
-    // setTimeout(function(){ 
+    // setTimeout(function(){
     //   conn.write("Move: left");
     // }, 2000);
 
-    // setTimeout(function(){ 
+    // setTimeout(function(){
     //   conn.write("Move: down");
     // }, 3000);
 
-    // setTimeout(function(){ 
+    // setTimeout(function(){
     //   conn.write("Move: right");
     // }, 4000);
-
-
-
   });
-
-
-
   
   //Update the connect function to also attach an event handler to handle incoming data and console log it for the player.
   conn.on('data', (data) => {
@@ -50,6 +42,6 @@ const connect = function() {
   });
 
   return conn;
-}
+};
 
 module.exports = { connect };
--- play.js
+++ play.js
@@ -3,3 +3,41 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+
+  handleUserInput();
+
+  stdin.resume();
+  return stdin;
+}
+
+
+
+const handleUserInput = function (){
+  const stdin = process.stdin;
+
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+
+  stdin.on('data', (key) => {
+      if (key === '\u0003') {
+          process.exit();
+      }
+
+      if (key === "1") {
+          console.log('Ayyyyy')
+      }
+      
+      console.log(key)
+  });
+};
+setupInput();
+
